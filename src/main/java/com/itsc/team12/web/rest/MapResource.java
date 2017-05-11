41d2e7347bf2913973a04539fa7821439e5ace18
JaCkIsO <samteblak@gmail.com>
2017-05-11 03:04:23 +0300

    Implement most REST Endpoints - All Map Endpoints Done

--- src/main/java/com/itsc/team12/web/rest/MapResource.java
+++ src/main/java/com/itsc/team12/web/rest/MapResource.java
@@ -1,25 +1,113 @@
 package com.itsc.team12.web.rest;
 
+import com.itsc.team12.entity.Map;
+import com.itsc.team12.repository.MapRepository;
+import com.itsc.team12.web.rest.util.HeaderUtil;
+import com.itsc.team12.web.rest.util.PlatformChecker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
 
 /**
  * Created by Sam on 5/10/2017.
  */
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/map")
 public class MapResource {
 
-    public ResponseEntity<?> checkVersion(){
-        return null;
+    @Autowired
+    private MapRepository mapRepository;
+
+    @Value("${root.directory}")
+    private String rootDirectory;
+
+    @RequestMapping(value = "/checkVersion", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<?> checkVersion() {
+
+        if (mapRepository.count() > 0) {
+
+            return new ResponseEntity<Object>(mapRepository.findTop1ByOrderByCreatedDesc(), HttpStatus.FOUND);
+        } else {
+            String empty = "Nothing to see here";
+            return new ResponseEntity<Object>(empty, HttpStatus.NOT_FOUND);
+        }
     }
 
-    public ResponseEntity<?> update(){
-        return null;
+    //  TODO Testing needed
+    @RequestMapping(value = "/update", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<?> update(HttpServletResponse response) {
+        if (mapRepository.count() > 0) {
+
+            Map result = mapRepository.findTop1ByOrderByCreatedDesc();
+            File file = new File(result.getFileName());
+            Path path = Paths.get(file.getAbsolutePath());
+            ByteArrayResource resource = null;
+            try {
+                resource = new ByteArrayResource(Files.readAllBytes(path));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return ResponseEntity.ok()
+                    .contentLength(file.length())
+                    .contentType(MediaType.parseMediaType("application/octet-stream"))
+                    .body(resource);
+
+        } else {
+            String empty = "Nothing to see here";
+            return new ResponseEntity<Object>(empty, HttpStatus.NOT_FOUND);
+        }
     }
 
-    public ResponseEntity<?> upload(){
-        return null;
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public ResponseEntity<?> upload(@RequestParam(value = "version") String version, @RequestParam("mapFile") MultipartFile mapFile) {
+        if (version == null || version.isEmpty()) {
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("Map", "noversion", "No Map version provided")).body(null);
+        }
+        String rootPath;
+        try {
+            if (mapFile.isEmpty()) {
+                return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("Map", "nomapfile", "No Map File provided")).body(null);
+            }
+            rootPath = rootDirectory + PlatformChecker.FILE_SEPARATOR + "Uploads";
+            File dir = new File(rootPath);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+            byte[] bytes = mapFile.getBytes();
+            File serverFile = new File(dir.getAbsoluteFile(), mapFile.getOriginalFilename());
+            BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(serverFile));
+            outputStream.write(bytes);
+            outputStream.close();
+        } catch (IOException e) {
+            String empty = "IO Error. Please inform admins";
+            return new ResponseEntity<Object>(empty, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        Map map = new Map();
+        map.setVersion(version);
+        map.setCreated(LocalDateTime.now());
+        map.setFileName(rootPath + PlatformChecker.FILE_SEPARATOR + mapFile.getOriginalFilename());
+        Map result = mapRepository.save(map);
+        return ResponseEntity.accepted()
+                .headers(HeaderUtil.createEntityCreationAlert("Map", result.getId().toString()))
+                .body(result);
     }
 }
--- src/main/java/com/itsc/team12/web/rest/UserResource.java
+++ src/main/java/com/itsc/team12/web/rest/UserResource.java
@@ -1,47 +1,146 @@
 package com.itsc.team12.web.rest;
 
+import com.itsc.team12.entity.Location;
+import com.itsc.team12.entity.User;
+import com.itsc.team12.repository.UserRepository;
+import com.itsc.team12.web.rest.util.HeaderUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by Sam on 5/10/2017.
  */
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/user")
 public class UserResource {
 
-    public ResponseEntity<?> register(){
+    @Autowired
+    private UserRepository userRepository;
+
+    //  TODO Testing needed
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<User> register(@RequestBody User user) throws URISyntaxException {
+        if (user.getId() != null) {
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("User", "idexists", "A new User cannot already have an ID")).body(null);
+        }
+        if (validator(user) != null) {
+            return validator(user);
+        }
+        User result = userRepository.save(user);
+
+        return ResponseEntity.created(new URI("/api/user/" + result.getId()))
+                .headers(HeaderUtil.createEntityCreationAlert("User", result.getId().toString()))
+                .body(result);
+    }
+
+    //  TODO Testing needed
+    @RequestMapping(value = "/auth", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<User> auth(@RequestBody User user) {
+        if (validator(user) != null) {
+            return validator(user);
+        }
+        User authUser = userRepository.findByDeviceId(user.getDeviceId());
+
+        return Optional.ofNullable(authUser)
+                .map(result -> new ResponseEntity<>(
+                        result,
+                        HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    //  TODO Testing needed
+    @RequestMapping(value = "/updateName", method = RequestMethod.PUT)
+    @ResponseBody
+    public ResponseEntity<User> updateName(@RequestBody User user) {
+        if (validator(user) != null) {
+            return validator(user);
+        }
+        User result = userRepository.findByDeviceId(user.getDeviceId());
+
+        if (result != null) {
+            result.setFirstName(user.getFirstName());
+            result.setLastName(user.getLastName());
+            userRepository.save(result);
+            return ResponseEntity.ok()
+                    .headers(HeaderUtil.createEntityUpdateAlert("User", result.getId().toString()))
+                    .body(result);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    public ResponseEntity<?> setLocation(String deviceId, Location location) {
         return null;
     }
 
-    public ResponseEntity<?> auth(){
+    public ResponseEntity<?> search(String firstName, String lastName) {
         return null;
     }
 
-    public ResponseEntity<?> updateName(){
-        return null;
+    //  TODO Testing needed
+    @RequestMapping(value = "/getAll", method = RequestMethod.GET)
+    @ResponseBody
+    public List<User> getAll() {
+
+        List<User> users = userRepository.findByIsVisible(true);
+        return users;
     }
 
-    public ResponseEntity<?> setLocation(){
-        return null;
+    //  TODO  Testing needed
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<?> get(@PathVariable String id) {
+        User user = userRepository.findOne(id);
+        return Optional.ofNullable(user)
+                .map(result -> new ResponseEntity<>(
+                        result,
+                        HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    public ResponseEntity<?> search(){
-        return null;
+    //  TODO Testing needed
+    @RequestMapping(value = "/toggleVisiblity", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<?> toggleVisiblity(@RequestBody User user) {
+        if (validator(user) != null) {
+            return validator(user);
+        }
+        User result = userRepository.findByDeviceId(user.getDeviceId());
+        if (result != null) {
+            if (result.isVisible()) {
+                result.setVisible(false);
+            } else {
+                result.setVisible(true);
+            }
+            userRepository.save(result);
+            return ResponseEntity.ok()
+                    .headers(HeaderUtil.createEntityUpdateAlert("User", result.getId().toString()))
+                    .body(result);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    public ResponseEntity<?> getAll(){
+    public ResponseEntity validator(User user) {
+        if (user.getDeviceId() == null || user.getDeviceId().isEmpty()) {
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("User", "nodeviceid", "No Device ID provided")).body(null);
+        }
+        if (user.getFirstName() == null || user.getFirstName().isEmpty()) {
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("User", "firstname", "No First Name provided")).body(null);
+        }
+        if (user.getLastName() == null || user.getLastName().isEmpty()) {
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("User", "lastname", "No Last Name provided")).body(null);
+        }
         return null;
     }
-
-    public ResponseEntity<?> get(){
-        return null;
-    }
-
-    public ResponseEntity<?> toggleVisiblity(){
-        return null;
-    }
-
 
 }
